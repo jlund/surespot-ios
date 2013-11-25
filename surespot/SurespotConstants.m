f67c8f5f889a9f18742d79c22a907d2832d1e675
Adam Patacchiola <adam@2fours.com>
2013-11-25 16:28:04 -0700

    leverage DEBUG ifdef for network config
    Former-commit-id: 91e2ce5779d306f158b5f0495e0b9e630d82713b

--- surespot/SurespotConstants.m
+++ surespot/SurespotConstants.m
@@ -9,14 +9,19 @@
 #import "SurespotConstants.h"
 
 @implementation SurespotConstants
-BOOL const serverSecure = YES;
-NSString * const serverBaseIPAddress = @"server.surespot.me";//@"192.168.10.68";
-NSInteger const serverPort = 443;
 
-NSString * const serverPublicKeyString = // @"-----BEGIN PUBLIC KEY-----\nMIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQA93Acih23m8Jy65gLo8A9t0/snVXe\nRm+6ucIp56cXPgYvBwKDxT30z/HU84HPm2T8lnKQjFGMTUKHnIW+vqKFZicAokkW\nJ/GoFMDGz5tEDGEQrHk/tswEysri5V++kzwlORA+kAxAasdx7Hezl0QfvkPScr3N\n5ifR7m1J+RFNqK0bulQ=\n-----END PUBLIC KEY-----"; //local
+#ifdef DEBUG
+    BOOL const serverSecure = NO;
+    NSString * const serverBaseIPAddress = @"192.168.10.68";
+    NSInteger const serverPort = 8080;
 
-
-@"-----BEGIN PUBLIC KEY-----\nMIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQA/mqxm0092ovWqQluMYWJXc7iE+0v\nmrA8vJNUo1bAEe9dWY9FucDnZIbNNNGKh8soA9Ej7gyW9Yc6D7llh52LhscBpGd6\nbX+FNZEROhIDJP2KgTTKVX+ASB0WtPT3V9AbyoAAxEse8IP5Wec5ZGQG1B/mOlGm\nZ/aaRkB1bwl9eCNojpw=\n-----END PUBLIC KEY-----"; //prod
+    NSString * const serverPublicKeyString =  @"-----BEGIN PUBLIC KEY-----\nMIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQA93Acih23m8Jy65gLo8A9t0/snVXe\nRm+6ucIp56cXPgYvBwKDxT30z/HU84HPm2T8lnKQjFGMTUKHnIW+vqKFZicAokkW\nJ/GoFMDGz5tEDGEQrHk/tswEysri5V++kzwlORA+kAxAasdx7Hezl0QfvkPScr3N\n5ifR7m1J+RFNqK0bulQ=\n-----END PUBLIC KEY-----"; //local
+#else
+    BOOL const serverSecure = YES;
+    NSString * const serverBaseIPAddress = @"server.surespot.me";
+    NSInteger const serverPort = 443;
+    NSString * const serverPublicKeyString = @"-----BEGIN PUBLIC KEY-----\nMIGbMBAGByqGSM49AgEGBSuBBAAjA4GGAAQA/mqxm0092ovWqQluMYWJXc7iE+0v\nmrA8vJNUo1bAEe9dWY9FucDnZIbNNNGKh8soA9Ej7gyW9Yc6D7llh52LhscBpGd6\nbX+FNZEROhIDJP2KgTTKVX+ASB0WtPT3V9AbyoAAxEse8IP5Wec5ZGQG1B/mOlGm\nZ/aaRkB1bwl9eCNojpw=\n-----END PUBLIC KEY-----"; //prod
+#endif
 
 NSInteger const SAVE_MESSAGE_COUNT = 50;
 
