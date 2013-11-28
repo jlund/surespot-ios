21ff611b709d36bb69366e5e8c0ac23444efdf58
Adam Patacchiola <adam@2fours.com>
2013-11-28 16:39:33 -0700

    integrate google drive sdk
    Former-commit-id: 5392815fa1bd95258d38aa7f462e8aadcad011e3

--- surespot/RestoreIdentityViewController.h
+++ surespot/RestoreIdentityViewController.h
@@ -9,5 +9,6 @@
 #import <UIKit/UIKit.h>
 
 @interface RestoreIdentityViewController : UIViewController
+- (IBAction)bLoadIdentities:(id)sender;
 
 @end
--- surespot/RestoreIdentityViewController.m
+++ surespot/RestoreIdentityViewController.m
@@ -7,9 +7,15 @@
 //
 
 #import "RestoreIdentityViewController.h"
+#import "GTLDrive.h"
+#import "GTMOAuth2ViewControllerTouch.h"
+
+static NSString *const kKeychainItemName = @"Google Drive surespot";
+static NSString *const kClientID = @"428168563991-rsb9bkasjio1lbh9s4rd8tmi189gfqv0.apps.googleusercontent.com";
+static NSString *const kClientSecret = @"fcqLFxhN1OxonKFIoJG3NcJA";
 
 @interface RestoreIdentityViewController ()
-
+@property (nonatomic, retain) GTLServiceDrive *driveService;
 @end
 
 @implementation RestoreIdentityViewController
@@ -26,7 +32,81 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	self.driveService = [[GTLServiceDrive alloc] init];
+    self.driveService.authorizer = [GTMOAuth2ViewControllerTouch authForGoogleFromKeychainForName:kKeychainItemName
+                                                                                         clientID:kClientID
+                                                                                     clientSecret:kClientSecret];
+    
+}
+
+
+// Helper to check if user is authorized
+- (BOOL)isAuthorized
+{
+    return [((GTMOAuth2Authentication *)self.driveService.authorizer) canAuthorize];
+}
+
+// Creates the auth controller for authorizing access to Google Drive.
+- (GTMOAuth2ViewControllerTouch *)createAuthController
+{
+    GTMOAuth2ViewControllerTouch *authController;
+    authController = [[GTMOAuth2ViewControllerTouch alloc] initWithScope:kGTLAuthScopeDriveFile
+                                                                clientID:kClientID
+                                                            clientSecret:kClientSecret
+                                                        keychainItemName:kKeychainItemName
+                                                                delegate:self
+                                                        finishedSelector:@selector(viewController:finishedWithAuth:error:)];
+    return authController;
+}
+
+// Handle completion of the authorization process, and updates the Drive service
+// with the new credentials.
+- (void)viewController:(GTMOAuth2ViewControllerTouch *)viewController
+      finishedWithAuth:(GTMOAuth2Authentication *)authResult
+                 error:(NSError *)error
+{
+    if (error != nil)
+    {
+        [self showAlert:@"Authentication Error" message:error.localizedDescription];
+        self.driveService.authorizer = nil;
+    }
+    else
+    {
+        self.driveService.authorizer = authResult;
+    }
+}
+
+// Helper for showing a wait indicator in a popup
+- (UIAlertView*)showWaitIndicator:(NSString *)title
+{
+    UIAlertView *progressAlert;
+    progressAlert = [[UIAlertView alloc] initWithTitle:title
+                                               message:@"Please wait..."
+                                              delegate:nil
+                                     cancelButtonTitle:nil
+                                     otherButtonTitles:nil];
+    [progressAlert show];
+    
+    UIActivityIndicatorView *activityView;
+    activityView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    activityView.center = CGPointMake(progressAlert.bounds.size.width / 2,
+                                      progressAlert.bounds.size.height - 45);
+    
+    [progressAlert addSubview:activityView];
+    [activityView startAnimating];
+    return progressAlert;
+}
+
+// Helper for showing an alert
+- (void)showAlert:(NSString *)title message:(NSString *)message
+{
+    UIAlertView *alert;
+    alert = [[UIAlertView alloc] initWithTitle: title
+                                       message: message
+                                      delegate: nil
+                             cancelButtonTitle: @"OK"
+                             otherButtonTitles: nil];
+    [alert show];
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +115,13 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)bLoadIdentities:(id)sender {
+    
+    if (![self isAuthorized])
+    {
+        // Not yet authorized, request authorization and push the login UI onto the navigation stack.
+        [self.navigationController pushViewController:[self createAuthController] animated:YES];
+    }
+
+}
 @end
