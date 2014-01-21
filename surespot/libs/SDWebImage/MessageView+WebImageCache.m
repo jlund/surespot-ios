9f1582221aef8df09652b7202770f1d669b9ecc2
Adam Patacchiola <adam@2fours.com>
2014-01-21 12:29:27 -0700

    Revert "remove unused crap"
    This reverts commit 9c9843ae5b03c44ba23ba85bcceede408ae29df7 [formerly 92e0c0f3fea0be7312bf6dc57bf6a5493107f6ce].
    
    
    Former-commit-id: 6d8503d5332a0f0d3143ec74aa184ab0a7f6bea6

--- surespot/libs/SDWebImage/MessageView+WebImageCache.m
+++ surespot/libs/SDWebImage/MessageView+WebImageCache.m
@@ -20,6 +20,9 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 static const int ddLogLevel = LOG_LEVEL_OFF;
 #endif
 
+static char operationKey;
+static char operationArrayKey;
+
 static const NSInteger retryAttempts = 5;
 
 @implementation MessageView (WebCache)
@@ -31,67 +34,98 @@ static const NSInteger retryAttempts = 5;
          completed:(SDWebImageCompletedBlock)completedBlock
       retryAttempt:(NSInteger) retryAttempt
 {
+    //[self cancelCurrentImageLoad];
+    
+    //    self.uiImageView.image = placeholder;
+    
     NSURL * url = [NSURL URLWithString:message.data];
     
     if (url)
     {
         __weak MessageView *wself = self;
-        [SDWebImageManager.sharedManager downloadWithURL: url
-                                                mimeType: [message mimeType]
-                                              ourVersion: [message getOurVersion]
-                                           theirUsername: [message getOtherUser]
-                                            theirVersion: [message getTheirVersion]
-                                                      iv: [message iv]
-                                                 options: SDWebImageRetryFailed
-                                                progress:progressBlock completed:^(id image, NSString * mimeType, NSError *error, SDImageCacheType cacheType, BOOL finished)
-         {
-             if (!wself) return;
-             dispatch_main_async_safe(^
-                                      {
-                                          if (!wself) return;
-                                          
-                                          //do nothing if the message has changed
-                                          if (![wself.message isEqual:message]) {
-                                              DDLogInfo(@"cell is pointing to a different message now, not assigning data");
-                                              return;
-                                          }
-                                          if (image)
-                                          {
-                                              if ([mimeType isEqualToString:MIME_TYPE_IMAGE]) {
-                                                  wself.uiImageView.image = image;
-                                                  
-                                                  
-                                                  if ([image size].height > [image size].width) {
-                                                      [wself.uiImageView setContentMode:UIViewContentModeScaleAspectFit];
-                                                  }
-                                                  else {
-                                                      [wself.uiImageView setContentMode:UIViewContentModeScaleAspectFill];
-                                                  }
-                                              }
-                                              if (message.formattedDate) {
-                                                  wself.messageStatusLabel.text = message.formattedDate;
-                                              }
-                                          }
-                                          else {
-                                              //retry
-                                              if (retryAttempt < retryAttempts) {
-                                                  DDLogInfo(@"no data downloaded, retrying attempt: %d", retryAttempt+1);
-                                                  [self setMessage:message progress:progressBlock completed:completedBlock retryAttempt:retryAttempt+1];
-                                                  return;
-                                              }
-                                              else {
-                                                  wself.messageStatusLabel.text = NSLocalizedString(@"error_downloading_message_data", nil);
-                                              }
-                                          }
-                                          
-                                          [wself setNeedsLayout];
-                                          if (completedBlock && finished)
-                                          {
-                                              completedBlock(image, mimeType, error, cacheType);
-                                          }
-                                      });
-         }];
+        id<SDWebImageOperation> operation = [SDWebImageManager.sharedManager downloadWithURL: url
+                                                                                    mimeType: [message mimeType]
+                                                                                  ourVersion: [message getOurVersion]
+                                                                               theirUsername: [message getOtherUser]
+                                                                                theirVersion: [message getTheirVersion]
+                                                                                          iv: [message iv]
+                                                                                     options: SDWebImageRetryFailed
+                                                                                    progress:progressBlock completed:^(id image, NSString * mimeType, NSError *error, SDImageCacheType cacheType, BOOL finished)
+                                             {
+                                                 if (!wself) return;
+                                                 dispatch_main_async_safe(^
+                                                                          {
+                                                                              if (!wself) return;
+                                                                              
+                                                                              //do nothing if the message has changed
+                                                                              if (![wself.message isEqual:message]) {
+                                                                                  DDLogInfo(@"cell is pointing to a different message now, not assigning data");
+                                                                                  return;
+                                                                              }
+                                                                              if (image)
+                                                                              {
+                                                                                  if ([mimeType isEqualToString:MIME_TYPE_IMAGE]) {
+                                                                                      wself.uiImageView.image = image;
+                                                                                      
+                                                                                      
+                                                                                      if ([image size].height > [image size].width) {
+                                                                                          [wself.uiImageView setContentMode:UIViewContentModeScaleAspectFit];
+                                                                                      }
+                                                                                      else {
+                                                                                          [wself.uiImageView setContentMode:UIViewContentModeScaleAspectFill];
+                                                                                      }                                   
+                                                                                  }
+                                                                                  if (message.formattedDate) {
+                                                                                      wself.messageStatusLabel.text = message.formattedDate;
+                                                                                  }
+                                                                              }
+                                                                              else {
+                                                                                  //retry
+                                                                                  if (retryAttempt < retryAttempts) {
+                                                                                      DDLogInfo(@"no data downloaded, retrying attempt: %d", retryAttempt+1);
+                                                                                      [self setMessage:message progress:progressBlock completed:completedBlock retryAttempt:retryAttempt+1];
+                                                                                      return;
+                                                                                  }
+                                                                                  else {
+                                                                                      wself.messageStatusLabel.text = NSLocalizedString(@"error_downloading_message_data", nil);
+                                                                                  }
+                                                                              }
+                                                                              
+                                                                              [wself setNeedsLayout];
+                                                                              if (completedBlock && finished)
+                                                                              {
+                                                                                  completedBlock(image, mimeType, error, cacheType);
+                                                                              }
+                                                                          });
+                                             }];
+        objc_setAssociatedObject(self, &operationKey, operation, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
+}
+
+
+- (void)cancelCurrentImageLoad
+{
+    // Cancel in progress downloader from queue
+    id<SDWebImageOperation> operation = objc_getAssociatedObject(self, &operationKey);
+    if (operation)
+    {
+        [operation cancel];
+        objc_setAssociatedObject(self, &operationKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+}
+
+- (void)cancelCurrentArrayLoad
+{
+    // Cancel in progress downloader from queue
+    NSArray *operations = objc_getAssociatedObject(self, &operationArrayKey);
+    for (id<SDWebImageOperation> operation in operations)
+    {
+        if (operation)
+        {
+            [operation cancel];
+        }
+    }
+    objc_setAssociatedObject(self, &operationArrayKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 @end
